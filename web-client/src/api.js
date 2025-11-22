afd14edacf9872e434c0b4f3356264cfad844844
HelloOjasMutreja <helloojasmutreja@gmail.com>
2025-11-22 23:49:25 +0530

    web-client setup complete

--- web-client/src/api.js
+++ web-client/src/api.js
@@ -1,3 +1,15 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://127.0.0.1:8000/api/' });
+
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api/',
+  // timeout: 10000,
+});
+
+// For dev only: attach BasicAuth easily when needed
+export function authConfig(username, password) {
+  return {
+    auth: { username, password }
+  };
+}
+
 export default api;
